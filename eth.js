61b990fcc663c12d907d1bd31545d85190877214
Stepan Peredriy <stepan.peredriy@icandeliver.ru>
2022-04-18 22:56:28 +0300

    After review

--- eth.js
+++ eth.js
@@ -11,33 +11,15 @@ const blockNumber = prompt('Input block number. For example: 14588452');
 const addr = prompt('Input address. For example: 0x66d91CD58D5E270AAa5525CaBc6cc6f41E9ed9e4');
 
 const Web3 = require("web3");
-const redis = require("redis");
-let redisClient = redis.createClient();
-
 const web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:8545"));
 
-let foundTransactions = [];
 (async () => {
-    const block = await web3.eth.getBlock(blockNumber);
+    const block = await web3.eth.getBlock(blockNumber, true);
     if (block != null  && block.transactions != null && block.transactions.length !== 0) {
         const txs = block.transactions;
-        for (const tx of txs) {
-            const data = await web3.eth.getTransaction(tx);
-            if ((data.from === addr) || (data.to === addr)) {
-                foundTransactions.push(data.hash);
-                // v1 await redisClient.lPush(addr, data.hash);
-                console.log('Find transaction ' + data.hash + ' for address ' + addr + ' in block ' + blockNumber);
-            }
-            //console.log(data.hash);
-        }
-        await store(foundTransactions)
+        const foundTxs = txs.filter(function (e) {
+            return e.from === addr || e.to === addr;
+        });
+        console.log(foundTxs);
     }
 })();
-
-
-async function store(foundTransactions) {
-    await redisClient.on('error', (err) => console.log('Redis Client Error', err));
-    await redisClient.connect();
-    await redisClient.hSet(addr, ...foundTransactions);
-    await redisClient.disconnect();
-}
